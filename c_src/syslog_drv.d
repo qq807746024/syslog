2f54dced91d741afb528fd99ead2947d33138e9a
qq807746024 <45820276+qq807746024@users.noreply.github.com>
2021-01-23 17:49:06 +0800

    first

---
+++ c_src/syslog_drv.d
@@ -0,0 +1,5 @@
+c_src/syslog_drv.o: c_src/syslog_drv.c \
+ /ErlangDev/erLang_R16B03/lib/erl_interface-3.7.15/include/ei.h \
+ /ErlangDev/erLang_R16B03/erts-5.10.4/include/erl_driver.h \
+ /ErlangDev/erLang_R16B03/erts-5.10.4/include/erl_int_sizes_config.h \
+ /ErlangDev/erLang_R16B03/erts-5.10.4/include/erl_drv_nif.h
